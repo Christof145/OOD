3f90922180fdf406027d76559d7aefd0a635e5fb
Christof145 <43606559+Christof145@users.noreply.github.com>
2023-01-11 10:35:50 +0100

    Revert "Fix File save method"
    This reverts commit bb32008914a4d1945a141cfc8eb785033cdf4437.

--- Jabberpoint_Sourcecode_Students/src/MenuController.java
+++ Jabberpoint_Sourcecode_Students/src/MenuController.java
@@ -34,7 +34,6 @@ public class MenuController extends MenuBar {
 	protected static final String NEXT = "Next";
 	protected static final String OPEN = "Open";
 	protected static final String PAGENR = "Page number?";
-	protected static final String GOTOINPUT = "Save file as:";
 	protected static final String PREV = "Prev";
 	protected static final String SAVE = "Save";
 	protected static final String VIEW = "View";
@@ -77,15 +76,8 @@ public class MenuController extends MenuBar {
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Accessor xmlAccessor = new XMLAccessor();
-				String userInput = JOptionPane.showInputDialog(GOTOINPUT, null);
 				try {
-					if (userInput == null || userInput.equals("")){
-						JOptionPane.showMessageDialog(parent, "File not saved!",
-								SAVEERR, JOptionPane.ERROR_MESSAGE);
-					} else {
-						String fileSaveName = userInput + ".xml";
-						xmlAccessor.saveFile(presentation, fileSaveName);
-					}
+					xmlAccessor.saveFile(presentation, SAVEFILE);
 				} catch (IOException exc) {
 					JOptionPane.showMessageDialog(parent, IOEX + exc, 
 							SAVEERR, JOptionPane.ERROR_MESSAGE);
